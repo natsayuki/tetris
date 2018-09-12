a7b4e0bff3b00edc470a5bc3b45d19a108967c33
the42ndturtle <the42ndturtle@gmail.com>
2018-09-11 22:52:53 -0400

    oops heroku dynamic port

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ io.on('connection', (socket) => {
 
 app.use(express.static("static"));
 
-// let port = process.env.PORT || 3000;
-let port = 3000;
+let port = process.env.PORT || 3000;
+// let port = 3000;
 
 server.listen(port, () => {
   console.log("server up on port 3000");
